eac411239bb942b4a1ca25d41798285fcb23b7fd
kacpiq1 <156676642+kacpiq1@users.noreply.github.com>
2024-08-08 01:44:08 +0200

    Update fuelscript.js

--- cenypaliw/fuelscript.js
+++ cenypaliw/fuelscript.js
@@ -507,5 +507,28 @@ function showPriceHistoryChart() {
         });
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the modal
+    var modal = document.getElementById("taxChangeModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user opens the page, open the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
         document.addEventListener('DOMContentLoaded', fetchData);
         loadThemeFromLocalStorage();
