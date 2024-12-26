8b00ef20f50a2dc9a5de6a879b9db7938878225d
KacpiQ <156676642+kacpiq1@users.noreply.github.com>
2024-12-26 14:03:13 +0100

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,31 @@
+import { account, OAuthProvider } from './appwrite'
+
+export const loginWithGoogle = async () => {
+  try {
+    await account.createOAuth2Session(OAuthProvider.Google)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const logoutUser = async () => {
+  try {
+    await account.deleteSession('current')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getUser = async () => {
+  try {
+    return await account.get()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
