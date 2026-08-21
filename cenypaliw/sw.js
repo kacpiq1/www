7b1580687d33218e45d02f7eeb3b938aeeda7c02
KacpiQ <156676642+kacpiq1@users.noreply.github.com>
2026-08-21 18:37:55 +0200

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ cenypaliw/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'ceny-paliw-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './cenypaliw/fuelstyle.css',
+  './cenypaliw/fuelscript.js'
+];
+
+// Instalacja Service Workera i zapisywanie plików do pamięci podręcznej
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Serwowanie plików z pamięci podręcznej, co przyspiesza działanie aplikacji
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Jeśli plik jest w pamięci, zwracamy go. Jeśli nie, pobieramy z sieci.
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Czyszczenie starych pamięci podręcznych przy aktualizacji plików
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
